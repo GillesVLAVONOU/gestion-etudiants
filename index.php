2086eb43839a717f26c51c53d321737c1118ac3f
GillesVLAVONOU <vmgilles853@gmail.com>
2026-04-23 13:48:09 +0100

    Ajout formulaire et style

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include 'conn.php';
+
+// Récupération des filières
+$stmt = $pdo->query("SELECT id, nom FROM filieres");
+$filieres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion Étudiants</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Ajouter un Étudiant</h1>
+    <form action="traitement.php" method="post">
+        <label for="nom">Nom:</label>
+        <input type="text" id="nom" name="nom" required><br><br>
+        <label for="prenom">Prénom:</label>
+        <input type="text" id="prenom" name="prenom" required><br><br>
+        <label for="filiere">Filière:</label>
+        <select id="filiere" name="filiere" required>
+            <option value="">Sélectionnez une filière</option>
+            <?php foreach ($filieres as $filiere): ?>
+                <option value="<?php echo $filiere['id']; ?>"><?php echo $filiere['nom']; ?></option>
+            <?php endforeach; ?>
+        </select><br><br>
+        <input type="submit" value="Ajouter">
+    </form>
+</body>
+</html>
